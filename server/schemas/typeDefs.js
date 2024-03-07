3f971ff3434ca5bad5d1face4b61a454449bf380
StefanPDavis <snoodle22@hotmail.com>
2024-03-07 10:48:31 -0800

    added typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const typeDefs = `
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        projects: [Project]!
+
+    }
+
+    type Project {
+        _id: ID
+        projectText: String
+        projectAuthor: String
+        comments: [Comment]!
+
+    }
+
+    type Comment {
+        _id: ID
+        commentText: String
+        commentAuthor: String
+
+    }
+
+    type Query {
+        users: [User]
+        user(username: String!): User
+        projects(username: String!): [Project]
+        project(project: ID!): Project
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        addProject(projectText: String!): Project
+        addComment(projectId: ID!, commentText: String!): Project
+        removeProject(projectId: ID!): Project
+        removeComment(projectId: ID!, commentId: ID!): Project
+    }
+`;
+
+
+module.exports = typeDefs;
