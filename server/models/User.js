9570bb2dc89998bd3b155b5e89c23c68dc331242
StefanPDavis <snoodle22@hotmail.com>
2024-03-07 11:41:20 -0800

    added userSchema to User model

---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  projects: [{
+    type: schema.Types.ObjectId,
+    ref: 'Project'
+  }]
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = process.env.SALTROUNDS;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
