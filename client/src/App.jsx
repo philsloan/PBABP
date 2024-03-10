4a13b559820a5a95fdb233cf08623eed1417e51a
StefanPDavis <snoodle22@hotmail.com>
2024-03-10 12:10:35 -0700

    imported footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
 // import Header from './components/Header';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
@@ -42,7 +42,7 @@ function App() {
         <div className="container">
           <Outlet />
         </div>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </ApolloProvider>
   );
